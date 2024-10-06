7598bf4c30dbf3ebb8814c529ad05817c42a0458
ewbyf <ewbyf@umsystem.edu>
2024-10-06 04:48:27 -0500

    added basic navbar and fixed some layout height stuff

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-white w-3/4 h-20 flex items-center fixed top-0 rounded-[100px] m-8 shadow px-16 z-50">
+            <p className="text-2xl font-bold">Codeban</p>
+        </nav>
+    );
+}
+ 
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,28 @@
 import GetStartedButton from '@/components/GetStartedButton';
 import GithubButton from '@/components/GithubButton';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
 	return (
-		<div className='flex flex-col justify-center items-center w-full h-screen'>
-			<div className='pattern-cross pattern-gray-400 pattern-bg-white pattern-size-8 pattern-opacity-10 h-screen absolute w-full'></div>
-			{/* <p className='text-5xl font-roboto font-bold'>Codeban</p> */}
+        <div>
+			<div className='pattern-cross pattern-gray-500 pattern-bg-gray-300 pattern-size-8 pattern-opacity-10 h-full absolute w-full'></div>
+            <div className='flex flex-col items-center w-full h-full'>
+                <Navbar></Navbar>
+                <section className='flex px-[20%] w-full h-screen items-center min-h-screen'>
+                    <div className='flex flex-col w-1/2 gap-4 items-start'>
+                        <p className='text-4xl font-bold'>
+                            Transform comments into{' '}
+                            <span className='bg-gradient-to-r from-[#ff00b2] to-[#60F] inline-block text-transparent bg-clip-text'>actionable</span> tasks
+                        </p>
+                        <p className='text-gray-500'>No more manual task creation—just code, comment, and let Codeban handle the rest for you.</p>
+                        <GetStartedButton></GetStartedButton>
+                    </div>
+                    <div>{/* img */}</div>
+                </section>
+                {/* <GithubButton></GithubButton> */}
+            </div>
+        </div>
 
-			<section className='flex px-[20%] w-full'>
-				<div className='flex flex-col w-1/2 gap-4 items-start'>
-					<p className='text-4xl font-bold'>
-						Transform comments into{' '}
-						<span className='bg-gradient-to-r from-[#ff00b2] to-[#60F] inline-block text-transparent bg-clip-text'>actionable</span> tasks
-					</p>
-					<p className='text-gray-500'>No more manual task creation—just code, comment, and let Codeban handle the rest for you.</p>
-                    <GetStartedButton></GetStartedButton>
-				</div>
-				<div>{/* img */}</div>
-			</section>
-			{/* <GithubButton></GithubButton> */}
-		</div>
 	);
 }
 
