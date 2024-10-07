7d870709fa381773b59cad09e2b8727ce56748da
ewbyf <ewbyf@umsystem.edu>
2024-10-07 02:41:17 -0500

    navbar update, added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 const Navbar = () => {
     return (
-        <nav className="bg-white w-3/4 h-20 flex items-center fixed top-0 rounded-[100px] m-8 shadow px-16 z-50">
-            <p className="text-2xl font-bold">Codeban</p>
+        <nav className="bg-white w-3/4 h-20 flex items-center fixed top-0 rounded-[100px] m-8 shadow px-12 z-50 gap-16">
+            <div className="flex items-center gap-2">
+                <img src="logo.png" alt="" height={60} width={60}/>
+                <p className="text-2xl font-bold">Codeban</p>
+            </div>
+            <a href="#" className="font-[500] hover:text-[#707070]">Try it</a>
+            <a href="#" className="font-[500] hover:text-[#707070]">Features</a>
+            <a href="#" className="font-[500] hover:text-[#707070]">How it works</a>
+            <a href="#" className="font-[500] hover:text-[#707070]">Benefits</a>
+            <a href="#" className="font-[500] hover:text-[#707070]">Testimonials</a>
+            <a href="#" className="font-[500] hover:text-[#707070]">FAQ</a>
         </nav>
     );
 }
