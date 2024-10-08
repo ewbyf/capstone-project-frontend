ea6a8dafffc5fb3380cd53c317b2dcdadd2f907e
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2024-10-08 11:37:57 -0500

    navbar change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
-        <nav className="bg-white w-3/4 h-20 flex items-center fixed top-0 rounded-[100px] m-8 shadow px-12 z-50 gap-16">
+        <nav className="bg-white w-full flex items-center fixed top-0 shadow px-12 py-2 z-50 gap-12">
             <div className="flex items-center gap-2">
-                <img src="logo.png" alt="" height={60} width={60}/>
+                <img src="logo.svg" alt="" height={60} width={60}/>
                 <p className="text-2xl font-bold text-[#3C3C3C]">Codeban</p>
             </div>
             <a href="#" className="font-inter font-[500] hover:text-[#4F46E5]">Try it</a>
