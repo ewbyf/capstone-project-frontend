bee742e4347142f1c32e01ac8a15162b1b476451
ewbyf <ewbyf@umsystem.edu>
2024-10-18 04:05:03 -0500

    kanban component

---
+++ src/pages/board.tsx
@@ -0,0 +1,11 @@
+import { KanbanBoard } from "@/components/KanbanBoard";
+
+const Board = () => {
+    return (
+        <div>
+            <KanbanBoard></KanbanBoard>
+        </div>
+    );
+}
+ 
+export default Board;
