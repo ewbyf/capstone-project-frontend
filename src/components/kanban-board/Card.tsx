34548bb7f0cf633f07cece40bcdc53b859ea02d9
ewbyf <ewbyf@umsystem.edu>
2024-11-14 04:35:59 -0600

    reorganized everything

---
+++ src/components/kanban-board/Card.tsx
@@ -0,0 +1,23 @@
+import { CardProps } from '@/interfaces/Kanban';
+import { motion } from 'framer-motion';
+import { DropIndicator } from './DropIndicator';
+
+export const Card = ({ title, id, column, handleDragStart }: CardProps) => {
+	return (
+		<>
+			<DropIndicator beforeId={id} column={column} />
+			<motion.div
+				layout
+				layoutId={id}
+				draggable='true'
+				onDragStart={(e) => handleDragStart(e, { title, id, column })}
+				className='cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing'
+			>
+				<p className='text-sm text-neutral-100'>{title}</p>
+			</motion.div>
+		</>
+	);
+};
+
+
+
--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,32 +1,70 @@
-import { KanbanBoard } from "@/components/KanbanBoard";
-import api from "@/services/axiosConfig";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { ListItem } from "@/interfaces/ListItem";
+import { CardType } from '@/interfaces/Kanban';
+import { ListItem } from '@/interfaces/ListItem';
+import api from '@/services/axiosConfig';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { BurnBarrel } from '@/components/kanban-board/BurnBarrel';
+import { Column } from '@/components/kanban-board/Column';
 
 const Board = () => {
-    const [id, setId] = useState('');
-    const [items, setItems] = useState<ListItem[]>([])
-    const router = useRouter();
+	const [id, setId] = useState('');
+	const [items, setItems] = useState<ListItem[]>([]);
+	const [cards, setCards] = useState(DEFAULT_CARDS);
+	const router = useRouter();
 
-    useEffect(() => {
-        const arr = router.asPath.split('=')
-        setId(arr[1]);
-        api.get(`/projects/${arr[1]}/todos?token=${localStorage.getItem('token')}`)
-        .then((resp) => {
-            console.log(resp.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+	useEffect(() => {
+		const arr = router.asPath.split('=');
+		setId(arr[1]);
+		api.get(`/projects/${arr[1]}/todos?token=${localStorage.getItem('token')}`)
+			.then((resp) => {
+				console.log(resp.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    }, [])
+	return (
+		<div className='h-screen w-full bg-background text-neutral-50'>
+			<div className='flex h-full w-full gap-3 overflow-scroll p-12'>
+				<Column title='Backlog' column='backlog' headingColor='text-neutral-500' cards={cards} setCards={setCards} />
+				<Column title='TODO' column='todo' headingColor='text-yellow-200' cards={cards} setCards={setCards} />
+				<Column title='In progress' column='doing' headingColor='text-blue-200' cards={cards} setCards={setCards} />
+				<Column title='Complete' column='done' headingColor='text-emerald-200' cards={cards} setCards={setCards} />
+				<BurnBarrel setCards={setCards} />
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div>
-            <KanbanBoard></KanbanBoard>
-        </div>
-    );
-}
- 
+const DEFAULT_CARDS: CardType[] = [
+	// BACKLOG
+	{ title: 'Look into render bug in dashboard', id: '1', column: 'backlog' },
+	{ title: 'SOX compliance checklist', id: '2', column: 'backlog' },
+	{ title: '[SPIKE] Migrate to Azure', id: '3', column: 'backlog' },
+	{ title: 'Document Notifications service', id: '4', column: 'backlog' },
+	// TODO
+	{
+		title: 'Research DB options for new microservice',
+		id: '5',
+		column: 'todo'
+	},
+	{ title: 'Postmortem for outage', id: '6', column: 'todo' },
+	{ title: 'Sync with product on Q3 roadmap', id: '7', column: 'todo' },
+
+	// DOING
+	{
+		title: 'Refactor context providers to use Zustand',
+		id: '8',
+		column: 'doing'
+	},
+	{ title: 'Add logging to daily CRON', id: '9', column: 'doing' },
+	// DONE
+	{
+		title: 'Set up DD dashboards for Lambda listener',
+		id: '10',
+		column: 'done'
+	}
+];
+
 export default Board;
